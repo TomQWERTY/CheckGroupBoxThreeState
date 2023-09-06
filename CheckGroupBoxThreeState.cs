ed1a381b0f6e534142220827147dc381d265c1ba
Oleksii Veselskyi <nightinzond@gmail.com>
2023-09-06 13:36:54 +0300

    Three state group checkbox added

---
+++ CheckGroupBoxThreeState.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace checkbox
+{
+    internal class CheckGroupBoxThreeState : GroupBox
+    {
+        CheckBox mainCB;
+        List<CheckBox> checkBoxes;
+
+        public CheckGroupBoxThreeState() : base()
+        {
+            checkBoxes = new List<CheckBox>();
+            mainCB = new CheckBox();
+            mainCB.CheckStateChanged += mainCB_CheckedChanged;
+            Controls.Add(mainCB);
+
+            ControlAdded += thisControlAdded;
+        }
+
+        private void ModifyMain(object sender, EventArgs e)
+        {
+            bool checkedPresent = false, uncheckedPresent = false;
+            foreach (CheckBox checkBox in checkBoxes)
+            {
+                if (checkBox.Checked)
+                {
+                    checkedPresent = true;
+                }
+                else if (!checkBox.Checked)
+                {
+                    uncheckedPresent = true;
+                }
+            }
+            if (checkedPresent && !uncheckedPresent)
+            {
+                mainCB.CheckState = CheckState.Checked;
+            }
+            else if (uncheckedPresent && !checkedPresent)
+            {
+                mainCB.CheckState = CheckState.Unchecked;
+            }
+            else
+            {
+                mainCB.CheckState = CheckState.Indeterminate;
+            }
+        }
+
+        private void thisControlAdded(object sender, ControlEventArgs e)
+        {
+            if (e.Control.GetType() == typeof(CheckBox))
+            {
+                (e.Control as CheckBox).CheckedChanged += ModifyMain;
+                checkBoxes.Add(e.Control as CheckBox);
+            }
+        }
+
+        private void mainCB_CheckedChanged(object sender, EventArgs e)
+        {
+            switch (mainCB.CheckState)
+            {
+                case CheckState.Checked:
+                    {
+                        foreach (CheckBox cb in checkBoxes)
+                        {
+                            cb.Checked = true;
+                        }
+                        break;
+                    }
+                case CheckState.Unchecked:
+                    {
+                        foreach (CheckBox cb in checkBoxes)
+                        {
+                            cb.Checked = false;
+                        }
+                        break;
+                    }
+            }
+        }
+    }
+}
---
+++ Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace checkbox
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            checkBox1 = new CheckBox();
+            groupBox1 = new CheckGroupBoxThreeState();
+            checkBox2 = new CheckBox();
+            checkBox4 = new CheckBox();
+            checkBox3 = new CheckBox();
+            groupBox1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.Location = new Point(6, 26);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(101, 24);
+            checkBox1.TabIndex = 0;
+            checkBox1.Text = "checkBox1";
+            checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(checkBox2);
+            groupBox1.Controls.Add(checkBox4);
+            groupBox1.Controls.Add(checkBox3);
+            groupBox1.Controls.Add(checkBox1);
+            groupBox1.Location = new Point(12, 12);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(250, 215);
+            groupBox1.TabIndex = 1;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "groupBox1";
+            // 
+            // checkBox2
+            // 
+            checkBox2.AutoSize = true;
+            checkBox2.Location = new Point(6, 116);
+            checkBox2.Name = "checkBox2";
+            checkBox2.Size = new Size(101, 24);
+            checkBox2.TabIndex = 2;
+            checkBox2.Text = "checkBox2";
+            checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox4
+            // 
+            checkBox4.AutoSize = true;
+            checkBox4.Location = new Point(6, 86);
+            checkBox4.Name = "checkBox4";
+            checkBox4.Size = new Size(101, 24);
+            checkBox4.TabIndex = 4;
+            checkBox4.Text = "checkBox4";
+            checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            checkBox3.AutoSize = true;
+            checkBox3.Location = new Point(6, 56);
+            checkBox3.Name = "checkBox3";
+            checkBox3.Size = new Size(101, 24);
+            checkBox3.TabIndex = 3;
+            checkBox3.Text = "checkBox3";
+            checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(288, 247);
+            Controls.Add(groupBox1);
+            Name = "Form1";
+            Text = "Form1";
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private CheckBox checkBox1;
+        private CheckGroupBoxThreeState groupBox1;
+        private CheckBox checkBox2;
+        private CheckBox checkBox4;
+        private CheckBox checkBox3;
+    }
+}
